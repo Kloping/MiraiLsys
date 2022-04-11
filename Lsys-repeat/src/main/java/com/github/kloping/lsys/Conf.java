cce6e5a7842bc8a884793aecf86bede7b82eb13c
HRS-Computer <3474006766@qq.com>
2022-04-11 21:32:27 +0800

    new module

--- Lsys-repeat/src/main/java/com/github/kloping/lsys/Conf.java
+++ Lsys-repeat/src/main/java/com/github/kloping/lsys/Conf.java
@@ -8,4 +8,12 @@ import java.util.List;
  */
 public class Conf {
     private List<Long> fathers = new ArrayList<>();
+
+    public List<Long> getFathers() {
+        return fathers;
+    }
+
+    public void setFathers(List<Long> fathers) {
+        this.fathers = fathers;
+    }
 }
--- Lsys-repeat/src/main/java/com/github/kloping/lsys/RepeatMethods.java
+++ Lsys-repeat/src/main/java/com/github/kloping/lsys/RepeatMethods.java
@@ -4,6 +4,10 @@ import cn.kloping.lsys.Resource;
 import cn.kloping.lsys.entitys.InvokeGroup;
 import cn.kloping.lsys.entitys.Result;
 import cn.kloping.lsys.workers.Methods;
+import net.mamoe.mirai.console.permission.AbstractPermitteeId;
+import net.mamoe.mirai.console.permission.PermissionService;
+import net.mamoe.mirai.console.permission.PermitteeId;
+import net.mamoe.mirai.event.events.GroupMessageEvent;
 
 /**
  * @author github.kloping
@@ -15,13 +19,15 @@ public class RepeatMethods {
         invokeGroup.getInvokes().put("跟我念.*", "repeat0");
         invokeGroup.getInvokesAfter().put("跟我念.*", new String[]{"$1"});
         Methods.invokes.put("repeat0", (user, request) -> {
-            if (user.getQq().longValue() == Resource.conf.getQq().longValue()) {
-                int i = request.getOStr().indexOf(".");
-                String str = request.getStr().substring(i);
-                return new Result(new Object[]{str}, 0);
-            } else {
-                return null;
+            if (request.getEvent() instanceof GroupMessageEvent) {
+                PermitteeId permitteeId = new AbstractPermitteeId.ExactMember(request.getGId().longValue(), request.getSendId().longValue());
+                if (PermissionService.hasPermission(permitteeId, Repeat.INSTANCE.getParentPermission().getId())) {
+                    int i = request.getOStr().indexOf(".");
+                    String str = request.getStr().substring(i);
+                    return new Result(new Object[]{str}, 0);
+                }
             }
+            return null;
         });
         Resource.loadConfAfter.add(() -> {
             if (!Resource.conf.getInvokeGroups().containsKey(invokeGroup.getId()))
