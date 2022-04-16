bec22a827354b442a6a2adc77c916dc514aac269
HRS-Computer <3474006766@qq.com>
2022-04-16 22:05:35 +0800

    new module

--- Lsys-repeat/src/main/java/com/github/kloping/lsys/RepeatMethods.java
+++ Lsys-repeat/src/main/java/com/github/kloping/lsys/RepeatMethods.java
@@ -2,12 +2,15 @@ package com.github.kloping.lsys;
 
 import cn.kloping.lsys.Resource;
 import cn.kloping.lsys.entitys.InvokeGroup;
-import cn.kloping.lsys.entitys.Result;
 import cn.kloping.lsys.workers.Methods;
 import net.mamoe.mirai.console.permission.AbstractPermitteeId;
 import net.mamoe.mirai.console.permission.PermissionService;
 import net.mamoe.mirai.console.permission.PermitteeId;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.message.data.MessageChain;
+import net.mamoe.mirai.message.data.MessageChainBuilder;
+import net.mamoe.mirai.message.data.PlainText;
+import net.mamoe.mirai.message.data.SingleMessage;
 
 /**
  * @author github.kloping
@@ -23,8 +26,18 @@ public class RepeatMethods {
                 PermitteeId permitteeId = new AbstractPermitteeId.ExactMember(request.getGId().longValue(), request.getSendId().longValue());
                 if (PermissionService.hasPermission(permitteeId, Repeat.INSTANCE.getParentPermission().getId())) {
                     int i = request.getOStr().indexOf(".");
-                    String str = request.getStr().substring(i);
-                    return new Result(new Object[]{str}, 0);
+                    String str = request.getStr().substring(0, i);
+                    MessageChain chain = request.getEvent().getMessage();
+                    MessageChainBuilder builder = new MessageChainBuilder();
+                    for (SingleMessage singleMessage : chain) {
+                        if (singleMessage instanceof PlainText) {
+                            PlainText text = (PlainText) singleMessage;
+                            String s0 = text.getContent().replace(str, "");
+                            singleMessage = new PlainText(s0);
+                        }
+                        builder.append(singleMessage);
+                    }
+                    ((GroupMessageEvent) request.getEvent()).getGroup().sendMessage(builder.build());
                 }
             }
             return null;
